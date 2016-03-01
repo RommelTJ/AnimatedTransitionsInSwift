5b10fd764abbb4846962b0113404a019deaf61a6
Rommel Rico <rommeltj@gmail.com>
2016-03-01 14:15:26 -0800

    Adding a custom segue

---
+++ AnimatedTransitionsInSwift/FirstHideViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstHideViewController.swift
+//  AnimatedTransitionsInSwift
+//
+//  Created by Rommel Rico on 3/1/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class FirstHideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- AnimatedTransitionsInSwift/FirstViewController.swift
+++ AnimatedTransitionsInSwift/FirstViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    //Properties
+    let transitionManager = TransitionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,6 +23,15 @@ class FirstViewController: UIViewController {
     //IBAction required to use an Exit Segue.
     @IBAction func unwindToViewController (sender: UIStoryboardSegue){
         //Code not necessary in our implementation.
+    }
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // this gets a reference to the screen that we're about to transition to
+        let toViewController = segue.destinationViewController as UIViewController
+        
+        //Instead of using the default transition animation, we'll ask the segue to 
+        //use our custom TransitionManager object to manage the transition animation.
+        toViewController.transitioningDelegate = self.transitionManager
     }
 
 }
