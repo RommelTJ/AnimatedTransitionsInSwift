354a681118522a16a349f6affd44b887b5353237
Rommel Rico <rommeltj@gmail.com>
2016-03-01 12:32:04 -0800

    Initial Commit

---
+++ AnimatedTransitionsInSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AnimatedTransitionsInSwift
+//
+//  Created by Rommel Rico on 3/1/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
